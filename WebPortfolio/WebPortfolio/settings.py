ea3a9d69d16e8e5c3d90cb94c5fabfe740e0da3a
Eduarda <eduardakb@hotmail.com>
2026-03-03 21:40:08 -0300

    Fix: Configurando exibição de imagens de mídia em produção

--- WebPortfolio/WebPortfolio/settings.py
+++ WebPortfolio/WebPortfolio/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^vby3hor%3k!c_o69-za*v)zd#o0ax96a5_u&vs%5vzw8^c5xm'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # settings.py
 
 ALLOWED_HOSTS = ['portfolio-wl3q.onrender.com', '127.0.0.1', 'localhost', '.onrender.com']
