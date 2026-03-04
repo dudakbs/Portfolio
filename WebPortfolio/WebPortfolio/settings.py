8d8a72d0c8252403c26ecb6363a1579a08561785
Eduarda <eduardakb@hotmail.com>
2026-03-03 21:15:27 -0300

    Fix: Adiciona gunicorn e corrige caminho static

--- WebPortfolio/WebPortfolio/settings.py
+++ WebPortfolio/WebPortfolio/settings.py
@@ -128,11 +128,13 @@ import os
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
